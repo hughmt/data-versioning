037c5136615c0eaa3937ce9f0b6d81561cc129ab
Hugh Taylor <hughmonrotaylor@gmail.com>
2024-11-06 17:28:55 -0500

    dataset updates...

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,6 +8,6 @@ mkdir -p /mnt/gcs_bucket
 gcsfuse --key-file=$GOOGLE_APPLICATION_CREDENTIALS $GCS_BUCKET_NAME /mnt/gcs_data
 echo 'GCS bucket mounted at /mnt/gcs_data'
 mkdir -p /app/cheese_dataset
-mount --bind /mnt/gcs_data/images /app/cheese_dataset
+mount --bind /mnt/gcs_data/dvc_store/images /app/cheese_dataset
 
 pipenv shell
